83c6be76f827f5467c2965c7d2a149e8523ee3c2
Manjunath G Chikkamath <gcmanjunath@zeomega.com>
2019-10-22 14:47:32 +0530

    dict file

---
+++ dict.py
@@ -0,0 +1,17 @@
+epsde_details={'name':'man','surname':'man1','addr':'123 abc'}
+print(epsde_details)
+
+
+input=[
+{'payload':'man','usecase':'man1','expected_code':'123 abc'}
+]
+
+output = process (input)
+
+output = {
+	,
+	'result':"pass/fail",
+	'actual_result': ""
+}
+
+
